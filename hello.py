467e4ac1ecda0ff3a97cd8367c59189d06f7fa2c
Kevin <kkangwfu@163.com>
2015-11-27 21:05:16 +0800

    learn first web app

--- hello.py
+++ hello.py
@@ -1,16 +1,22 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import redirect
+from flask.ext.script import Manager
 
+
+app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+	return redirect('http://www.google.com')
 
-
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+@app.route('/user/<id>')
+def get_user(id):
+	user = load_user(id)
+	if not user:
+		abort(404)
+	return '<h1>Hello, %s!</h1>' % user.name
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	manager.run()
